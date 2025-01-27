53de2af1feadc696d0acb0dbc52739f243f08121
BulbXML <baileytibbles2@gmail.com>
2025-01-26 17:32:54 -0700

    Modified TransformController script (see desc)
    Modified TransformController script to allow for both X and Y movement, as well as rotation on all three axes, all transforms controlled by SerializeFields for ease of access

--- GameObjectsAndTransformLab/Assets/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/TransformController.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
+    [SerializeField] private float xSpeed = 2.0f;
+    [SerializeField] private float ySpeed = 1.0f;
+    [SerializeField] private float xRot = 15.0f;
+    [SerializeField] private float yRot = 30.0f;
+    [SerializeField] private float zRot = 45.0f;
     // Start is called before the first frame update
     private void Update()
     {
         // MOVEMENT
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
+        // Speed controlled by xSpeed and ySpeed floats
+        var x = Mathf.PingPong(Time.time, xSpeed);
+        var y = Mathf.PingPong(Time.time, ySpeed);
+        // Horizontal movement created as x, combined with vertical movement (changed to y)
+        var p = new Vector3(x, y, 0);
         transform.position = p;
 
         // ROTATION
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        // Rotation speeds controlled by xRot, yRot and zRot floats
+        transform.Rotate(new Vector3(xRot, yRot, zRot) * Time.deltaTime);
     }
 }
