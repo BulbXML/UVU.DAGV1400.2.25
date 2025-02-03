1addd4a72ec26afa62e13c037758cc0bf6090d4b
BulbXML <baileytibbles2@gmail.com>
2025-02-02 17:25:58 -0700

    Created Collisions RB project

---
+++ Colliders RB/Assets/Shmovin.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shmovin : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    private Rigidbody rb;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(Vector3.right * speed);
+    }
+}
